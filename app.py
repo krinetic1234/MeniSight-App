e8655165d1c031df31886fb29ef69c1e170d794e
krinetic1234 <56781484+krinetic1234@users.noreply.github.com>
2021-02-25 00:03:47 -0800

    Flask detect

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request
+import os
+from joblib import dump, load
+import tensorflow as tf
+import keras
+from keras.models import load_model
+import pandas as pd
+import cv2
 
 app = Flask(__name__)
 
+detection_model = load_model('detection_weights.h5')
+
+UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+DETECTION = 'detection.html'
 
 
 def allowed_file(filename):
@@ -19,19 +31,19 @@ def detection():
     if request.method == 'POST':
         if(request.form["type"] == "0"):
             if 'image' not in request.files:
-                return render_template('hurricanes.html', output="File not found! Please try re-uploading.")
+                return render_template(DETECTION, output="File not found! Please try re-uploading.")
             f = request.files["image"]
             if f.filename == '':
-                return render_template('hurricanes.html', output="File not found! Please try re-uploading.")
+                return render_template(DETECTION, output="File not found! Please try re-uploading.")
             if f and allowed_file(f.filename):
-                filename = secure_filename(f.filename)
+                filename = f.filename
                 filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 f.save(filepath)
                 image = cv2.imread(filepath, cv2.IMREAD_COLOR)
                 image = cv2.resize(image, (300, 300))
                 image_tensor = tf.convert_to_tensor(image, dtype=tf.float32)
                 image_tensor = tf.expand_dims(image_tensor, 0)
-                result = hurricane_model.predict(image_tensor)[0][0]
+                result = detection_model.predict(image_tensor)[0][0]
                 print(result)
                 result = result * 100
                 if(result < 0):
@@ -40,13 +52,13 @@ def detection():
                     result = 100
                 result = round(result, 2)
                 result = str(result)
-                output = result + "% chance of a flood damage after a hurricane"
-                return render_template('detection.html', output=output)
-            return render_template('detection.html', output="An unknown error occurred!")
+                output = result + "% chance of tumor"
+                return render_template(DETECTION, output=output)
+            return render_template(DETECTION, output="An unknown error occurred!")
         else:
-            return render_template('detection.html', output="Coming soon!")
+            return render_template(DETECTION, output="Coming soon!")
     else:
-        return render_template('detection.html', output="")
+        return render_template(DETECTION, output="")
 
 
 @app.route('/prognosis')
